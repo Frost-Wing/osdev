b2f646d01d4284fed7c6dfc7e87fa1b0dd0ce12d
pradosh-arduino <pradoshnathan@gmail.com>
2023-11-09 13:03:49 +0530

    Moved kernel files to specific directories with their language names.

---
+++ source/kernel/typescript/typescript-source.c
@@ -0,0 +1,11 @@
+#include <kernel.h>
+#include <graphics.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+int typescript_main(void) {
+    print("Hello from type script!\n");
+
+    return 0;
+}
