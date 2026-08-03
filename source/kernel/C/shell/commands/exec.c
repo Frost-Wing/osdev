0e53887b06882264d2ddba619f57d976529d017c
pradosh-arduino <pradoshnathan@gmail.com>
2026-08-03 20:42:14 +0530

    Modified exec.c to spawn a process to run rather than relying on kernel.

--- source/kernel/C/shell/commands/exec.c
+++ source/kernel/C/shell/commands/exec.c
@@ -13,6 +13,8 @@
 #include <executables/elf.h>
 #include <graphics.h>
 #include <keyboard.h>
+#include <multitasking.h>
+#include <strings.h>
 #include <tty.h>
 #include <userland.h>
 
@@ -30,12 +32,14 @@ int cmd_exec(int argc, char **argv) {
     const char *user_argv[34];
     int user_argc = 0;
 
-    // argv[0] should be the program name (basename)
-    const char *basename = vfs_basename(path);
-    // user_argv[user_argc++] = basename;
-
-    // copy actual arguments AFTER the program path
-    for (int i = 2; i < argc && user_argc < 33; ++i)
+    /*
+     * Toybox selects an applet from argv[0] when invoked as
+     * `/bin/toybox <applet>`, so keep the historical shell behaviour of
+     * passing only the words after the executable path. For example:
+     *   exec /bin/toybox whoami  -> argv[0] = "whoami"
+     *   exec /bin/toybox sh      -> argv[0] = "sh"
+     */
+    for (int i = 2; i < argc && user_argc < 31; ++i)
         user_argv[user_argc++] = argv[i];
 
     user_argv[user_argc] = NULL;
@@ -43,23 +47,40 @@ int cmd_exec(int argc, char **argv) {
     tty_flush_input();
     keyboard_flush_buffer();
 
-    userland_exec_ctx_t ctx;
-
-    ctx.path = path;
-    ctx.argc = user_argc;
-    ctx.envp = NULL;
+    user_task_spec_t spec;
+    memset(&spec, 0, sizeof(spec));
+    spec.path = path;
+    spec.argc = user_argc;
+    spec.parent_pid = 0;
 
     for (int i = 0; i < user_argc; i++)
-        ctx.argv[i] = user_argv[i];
+        spec.argv[i] = user_argv[i];
 
-    ctx.argv[user_argc] = NULL;
-
-    if (userland_exec(&ctx) != 0) {
+    uint32_t pid = multitasking_spawn_userland(path, &spec);
+    if (!pid) {
         keyboard_flush_buffer();
-        eprintf("exec: failed to load ELF");
+        eprintf("exec: failed to spawn process");
         return -1;
     }
 
+    int exit_code = 0;
+    while (true) {
+        task_info_t info;
+        if (!multitasking_get_task(pid, &info))
+            break;
+
+        if (info.state == TASK_STATE_EXITED) {
+            task_info_t reaped;
+            if (multitasking_reap_task(pid, &reaped))
+                exit_code = reaped.exit_code;
+            else
+                exit_code = info.exit_code;
+            break;
+        }
+
+        multitasking_pump();
+    }
+
     keyboard_flush_buffer();
-    return 0;
+    return exit_code;
 }
